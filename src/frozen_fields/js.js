55ec3fd758985b89cafc1f25fb5f5b0e99d12993
Arthur Milchior <Arthur@Milchior.fr>
2020-04-27 20:56:11 +0200

    set back old star position

--- src/frozen_fields/js.js
+++ src/frozen_fields/js.js
@@ -19,15 +19,16 @@ function setFrozenFields(frozen) {
     frozenFields = frozen;
     $fnames = $(".fname");
     for (var i=0; i<frozen.length; i++) {
-        var $td_name = $(`#name${i}`);
-        if ($td_name.length == 0) {
-            // no multi column. Get the i-th fname
-            var td_name = $fnames[i];
-            $td_name = $(td_name);
-        }
+        var $div_field = $(`#f${i}`);
         var src = (frozen[i])?src_frozen:src_unfrozen;
         var un_freeze = (frozen[i])?"Unfreeze":"Freeze";
-        var div = `<img id=i${i} src='${src}' title='${un_freeze} field (${hotkey_toggle_field})' style="height:.9em" onclick='onFrozen(${i});'/>`;
-        $td_name.prepend(div);
+        var img = `<td style="width:28px"><img id=i${i} src='${src}' title='${un_freeze} field (${hotkey_toggle_field})' onclick='onFrozen(${i});'/></td>`;
+        $td_field = $div_field.parent();
+        $tr_field = $td_field.parent();
+        $tr_field.prepend(img);
+
+        var $td_name = $(`#name${i}`);
+        var $tr_name = $td_name.parent();
+        $tr_name.prepend("<td style='width:28px'></td>");
     }
 }
